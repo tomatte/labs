b85233c342b4d16047f7135662b6b51e6bc53fa9
tomatte <daniel.brandao2000@gmail.com>
2023-01-11 18:27:03 -0300

    receives data from shared memory and recreates the dictionary

---
+++ src/decoder/others/recreate_dictionary.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   recreate_dictionary.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbrandao <dbrandao@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 17:30:28 by dbrandao          #+#    #+#             */
+/*   Updated: 2023/01/11 18:21:20 by dbrandao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../decoder.h"
+
+static t_lst	*get_letters(char *data)
+{
+	t_lst	*dictionary;
+	char	*letters;
+	int		i;
+
+	letters = data + strlen(data) + 1;
+	i = 0;
+	dictionary = lst_new(letters[i]);
+	while (letters[++i])
+		lst_push(dictionary, lst_new(letters[i]));
+	return (dictionary);
+}
+
+static void	asign_codes(char *data, t_lst *dictionary)
+{
+	int	num;
+	int	i;
+
+	data += strlen(data) + 1;
+	num = strlen(data);
+	data += strlen(data) + 1;
+	i = 0;
+	while (i < num)
+	{
+		dictionary->code = ft_strdup(data);
+		dictionary = dictionary->next;
+		data += ft_strlen(data) + 1;
+		i++;
+	}
+}
+
+t_lst	*recreate_dictionary(char *data)
+{
+	t_lst	*dictionary;
+
+	dictionary = get_letters(data);
+	asign_codes(data, dictionary);
+	return (dictionary);
+}
